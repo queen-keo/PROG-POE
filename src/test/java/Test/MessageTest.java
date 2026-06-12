22a203d19af0f86a7aa6a746ecc58b970d97668b
Keorapetse <ST10474361@rcconnect.edu.za>
2026-06-12 15:28:00 +0200

    Update MessageTest.java

--- src/test/java/Test/MessageTest.java
+++ src/test/java/Test/MessageTest.java
@@ -39,12 +39,20 @@ public class MessageTest {
     }
 
     @Test
-    public void testMessageHashGenerate() {
+    public void testMessageHashGenerateP2() {
         // Data from Test Case 1
         // For ID to start with '00' for test verification
         String hash = msg.createMessageHash();
         assertTrue(hash.startsWith("00:"), "Message hash should start with 00:");
         assertTrue(hash.contains("HI") && hash.contains("MIKE"), "Hash should contain first and last word");
+    }
+    
+    @Test
+    public void testMessageHashGenerate() {
+
+        String hash = msg.createMessageHash();
+        assertNotNull(hash);
+        assertTrue(hash.contains(":"));
     }
     
     @Test
@@ -68,7 +76,7 @@ public class MessageTest {
     }
  
     @Test
-    public void testMessageHash() {
+    public void testMessageHashP2() {
         // Using Test Data 1: "HI MIKE, can you join us for dinner tonight?"
         String hash = msg.createMessageHash();
         
@@ -77,4 +85,108 @@ public class MessageTest {
         assertTrue(hash.contains(":"), "Hash must contain colons");
         assertTrue(hash.endsWith("HIMIKE"), "Hash should end with words in CAPS");
     }
+    
+    @Test
+    public void testMessageHash() {
+
+        String hash = msg.createMessageHash();
+        assertNotNull(hash);
+        assertTrue(hash.contains(":"));
+        assertTrue(hash.endsWith("HIMIKE"));
+}
+    
+    @Test
+    public void testStoredMessagesArray() {
+
+        Message msg1 = new Message(
+            "+27838884567",
+            "Where are you? You are late! I have asked you to be on time."
+    );
+
+        Message.addStoredMessage(msg1);
+        String result =
+            Message.searchMessagesByRecipient(
+                    "+27838884567"
+            );
+        assertTrue(
+            result.contains(
+                    "Where are you? You are late! I have asked you to be on time."
+            )
+    );
+}
+
+    @Test
+    public void testLongestMessage() {
+
+        Message msg1 = new Message(
+            "+27838884567",
+            "Hi"
+    );
+
+        Message msg2 = new Message(
+            "+27838884567",
+            "Where are you? You are late! I have asked you to be on time."
+    );
+
+        Message.addStoredMessage(msg1);
+        Message.addStoredMessage(msg2);
+        assertEquals(
+            "Where are you? You are late! I have asked you to be on time.",
+            Message.displayLongestMessage()
+    );
+}
+
+    @Test
+    public void testSearchByMessageID() {
+
+        Message msg = new Message(
+            "+27838884567",
+            "Testing message ID"
+    );
+
+        Message.addStoredMessage(msg);
+        String result =
+            Message.searchMessageByID(
+                    msg.getMessageID()
+            );
+        assertTrue(
+            result.contains(
+                    "Testing message ID"
+            )
+    );
+}
+
+    @Test
+    public void testDeleteMessageByHash() {
+
+        Message msg = new Message(
+            "+27838884567",
+            "Delete me"
+    );
+
+        Message.addStoredMessage(msg);
+        String result =
+            Message.deleteMessageByHash(
+                    msg.getMessageHash()
+            );
+        assertTrue(
+            result.contains(
+                    "successfully deleted"
+            )
+    );
+}
+
+    @Test
+    public void testDisplayReport() {
+
+        Message msg = new Message(
+            "+27838884567",
+            "Report Message"
+    );
+
+        Message.addStoredMessage(msg);
+        assertDoesNotThrow(() -> {
+        Message.displayReport();
+    });
+}
 }
